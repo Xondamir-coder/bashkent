2e945e3da6a645c78c0989e519ee1bc7f767fb01
Xondamir <xondamirnazrullayev@gmail.com>
2025-08-18 20:55:40 +0500

    colors, font (Gotham, Inter)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/google-fonts'],
   css: ['~/assets/scss/main.scss'],
+  googleFonts: {
+    display: 'swap',
+    families: {
+      Inter: [400, 500, 600, 700]
+    }
+  },
   vite: {
     css: {
       preprocessorOptions: {
